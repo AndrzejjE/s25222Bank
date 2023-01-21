6b311f995a95cf8e3feb0d395b88fcae325de66b
Andrzej Ebertowski <andrzeje@MacBook-Pro-Andrzej.local>
2023-01-21 09:43:54 +0100

    Initial commit

---
+++ src/main/java/com/example/s25222bank/S25222BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s25222bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S25222BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S25222BankApplication.class, args);
+    }
+
+}
